2e3f87559c026630885930537ca092fab48517a0
johnHackworth <javieralvarezlop@gmail.com>
2014-06-16 00:16:12 +0200

    spanish by default

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var _ = require('underscore');
     this.initialize(options);
   };
   app.prototype = {
-    language: 'en',
+    language: 'es',
     initialize: function(imports) {
       this.imports = imports;
       this.express = imports.express();
